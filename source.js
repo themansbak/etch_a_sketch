429422cd7548f33ae331fcd36a7c1eb9bc15bed9
aman <man.alex1995@gmail.com>
2020-03-26 21:45:55 -0700

    Randomized color and added shading

--- source.js
+++ source.js
@@ -1,19 +1,57 @@
+/*
+Modify on hover to - the cell color by 10%;
+*/
 const container = document.querySelector('.container');
+const resizeButton = document.querySelector('#resize-button');
+const promptButton = document.querySelector('#prompt-button');
+const input = document.querySelector('#resize-input');
 createCells();
 
-function submitInput() {
-  createCells(100);
-}
+input.addEventListener('keypress', function(e) {
+  if (e.key === 'Enter') submitInput(input.value);
+});
+resizeButton.addEventListener('click', () => {
+  submitInput(input.value);
+});
 
+function popupInput() {
+  const size = prompt('Enter a number between 1-100', '16');
+  if (size < 1 || size > 100) popupInput();
+  else createCells(size);
+}
+function submitInput(size) {
+  if (size < 1 || size > 100) alert('Enter a number between 1-100');
+  else createCells(size);
+  return;
+}
+function stringifyHSL(h, s, l) {
+  return 'hsl('+h+','+s+'%,'+l+'%)';
+}
+function stringifyRGB(r, g, b) {
+  return 'rgb('+r+','+g+','+b+')';
+}
 function addClass() {
   const cells = document.querySelectorAll('.cell');
   cells.forEach(cell => {
     cell.addEventListener('mouseenter', e=> {
-      cell.style.backgroundColor='black';
+      var cellColor = cell.style.backgroundColor;
+      if (!cellColor) {
+        cell.style.backgroundColor = stringifyRGB(Math.ceil(Math.random()*255),
+            Math.ceil(Math.random()*255), Math.ceil(Math.random()*255));
+      } else {
+        let color = cellColor.slice(4,-1).split(/[\s,]+/),
+          r = color[0],
+          g = color[1]
+          b = color[2];
+        const shadeFactor = 0.2;
+        r = r * (1-shadeFactor);
+        g = g * (1-shadeFactor);
+        b = b * (1-shadeFactor);
+        cell.style.backgroundColor = stringifyRGB(r,g,b);
+      }
     });
   });
 }
-
 function createCells(numCells=16) {
   container.innerHTML = '';
   container.style.gridTemplateColumns = 'repeat('+numCells+',1fr)';
